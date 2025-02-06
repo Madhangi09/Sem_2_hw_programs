f8a00077c7ae7de4f5889aeac4cf8f126418e9f1
Madhangi09 <kamatchibala6@gmail.com>
2025-02-06 18:54:13 +0530

    Add files via upload

---
+++ hw_06_02_25.py
@@ -0,0 +1,16 @@
+def long_palindrome(s):
+    if not s:
+        return ""
+    def expand_around_center(left,right):
+        while left>=0 and right<len(s) and s[left]==s[right]:
+            left-=1
+            right+=1
+        return s[left+1:right]
+    longest=""
+    for i in range(len(s)):
+        odd_palindrome=expand_around_center(i,i)
+        even_palindrome=expand_around_center(i,i+1)
+        longest=max(longest,odd_palindrome,even_palindrome,key=len)
+    return longest
+print(long_palindrome("babad"))  
+print(long_palindrome("cbbd"))   
