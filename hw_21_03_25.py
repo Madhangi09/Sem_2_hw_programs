0724de769ccf7b460642aa7303811aaddefb65da
Madhangi09 <kamatchibala6@gmail.com>
2025-03-24 20:51:55 +0530

    Add files via upload

---
+++ hw_21_03_25.py
@@ -0,0 +1,11 @@
+# Input strings
+str1 = "Hello"
+str2 = "World"
+
+# Swapping without a third variable
+str1, str2 = str2, str1
+
+# Output the swapped values
+print("After swapping:")
+print("str1 =", str1)
+print("str2 =", str2)
